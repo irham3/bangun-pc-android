337cd95cca947bd3bf7c9779cb27f864a22314b9
Wira Megantara Pamungkas <89589061+pamungkaswira27@users.noreply.github.com>
2023-06-12 11:32:33 +0700

    Improving ServiceScreen

---
+++ app/src/main/java/com/kaizen/bangunpc/ui/screen/auth/register/RegisterScreen.kt
@@ -0,0 +1,8 @@
+package com.kaizen.bangunpc.ui.screen.auth.register
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RegisterScreen() {
+
+}
--- app/src/main/java/com/kaizen/bangunpc/ui/screen/service/ServiceScreen.kt
+++ app/src/main/java/com/kaizen/bangunpc/ui/screen/service/ServiceScreen.kt
@@ -1,31 +1,32 @@
 package com.kaizen.bangunpc.ui.screen.service
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -35,7 +36,7 @@ import com.kaizen.bangunpc.ui.components.CircleButton
 import com.kaizen.bangunpc.ui.components.CustomTopBar
 import com.kaizen.bangunpc.ui.components.ScreenTitleText
 import com.kaizen.bangunpc.ui.theme.AppTheme
-import com.kaizen.bangunpc.utils.toRupiahFormat
+import com.kaizen.bangunpc.ui.theme.Orange
 
 @Composable
 fun ServiceScreen(
@@ -76,23 +77,52 @@ fun ServiceScreen(
                 modifier = modifier
                     .fillMaxWidth()
             ){
-                CircleButton(
-                    modifier = modifier.padding(8.dp),
-                    onClick = { },
-                    icon = {
-                        Image(
-                            painter = painterResource(id = R.drawable.tokopedia_logo),
-                            contentScale = ContentScale.Crop,
-                            contentDescription = stringResource(R.string.tokopedia_logo),
-                        )
-                    },
-                    buttonColors = ButtonDefaults.buttonColors(
-                        backgroundColor = Color.Transparent
-                    )
+                BorderCircularButton(
+                    linkUrl = stringResource(R.string.gxcomp_tokopedia),
+                    imageIcon = painterResource(id = R.drawable.tokopedia_logo)
+                )
+                BorderCircularButton(
+                    linkUrl = stringResource(R.string.gxcomp_shopee),
+                    imageIcon = painterResource(id = R.drawable.shopee_logo)
+                )
+                BorderCircularButton(
+                    linkUrl = stringResource(R.string.gxcomp_whatsapp),
+                    imageIcon = painterResource(id = R.drawable.whatsapp_logo)
                 )
             }
         }
     }
+}
+
+@Composable
+private fun BorderCircularButton(
+    modifier: Modifier = Modifier,
+    linkUrl: String,
+    imageIcon: Painter,
+    ctx: Context = LocalContext.current
+) {
+    CircleButton(
+        modifier = modifier
+            .padding(8.dp)
+            .border(BorderStroke(2.dp, Orange), CircleShape),
+        onClick = {
+            val urlIntent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse(linkUrl)
+            )
+            ctx.startActivity(urlIntent)
+        },
+        icon = {
+            Image(
+                painter = imageIcon,
+                contentScale = ContentScale.Fit,
+                contentDescription = "Link Toko",
+            )
+        },
+        buttonColors = ButtonDefaults.buttonColors(
+            backgroundColor = Color.Transparent
+        )
+    )
 }
 
 @Preview(
